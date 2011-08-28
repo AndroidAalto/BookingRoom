862a0e1d32d782edd4413966293ef1178d508a90
jush <oni.pool@gmail.com>
2011-08-28 17:00:53 +0800

    License change to GPL v3

--- src/org/androidaalto/bookingroom/view/WeekView.java
+++ src/org/androidaalto/bookingroom/view/WeekView.java
@@ -5,7 +5,7 @@
 
    BookingRoom is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
 
    BookingRoom is distributed in the hope that it will be useful,
@@ -14,8 +14,12 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with BookingRoom; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+   along with BookingRoom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * Notice: Code modified from com.android.calendar.CalendarView class.
+ *         The original code can be found at: http://android.git.kernel.org/?p=platform/packages/apps/Calendar.git;a=summary
  */
 
 package org.androidaalto.bookingroom.view;
