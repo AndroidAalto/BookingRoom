537cb54a337cbeeadaf0b22c47d585ace61f071e
Ramon <oni.pool@gmail.com>
2011-09-04 19:14:48 +0300

    Use constants for extra info at intents and code formatting

--- src/org/androidaalto/bookingroom/MeetingActivity.java
+++ src/org/androidaalto/bookingroom/MeetingActivity.java
@@ -28,47 +28,56 @@ import android.widget.TimePicker;
 
 public class MeetingActivity extends Activity {
 
+    public static final String EXTRA_END_TIME = "end";
+    public static final String EXTRA_CONTACT_EMAIL = "email";
+    public static final String EXTRA_CONTACT_NAME = "name";
+    public static final String EXTRA_TITLE = "title";
+    public static final String EXTRA_START_HOUR = "hour";
+    public static final String EXTRA_DAY = "day";
+    public static final String EXTRA_START_TIME = "start";
+
     EditText titleEdit, nameEdit, emailEdit;
     TimePicker startPicker, endPicker;
     TextView meetingHeader;
-    
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meeting);
-        
-        meetingHeader   = (TextView)    findViewById(R.id.meetingHeader);
-        startPicker     = (TimePicker)  findViewById(R.id.startPicker);
-        endPicker       = (TimePicker)  findViewById(R.id.endPicker);
-        titleEdit       = (EditText)    findViewById(R.id.titleEdit);
-        nameEdit        = (EditText)    findViewById(R.id.nameEdit);
-        emailEdit       = (EditText)    findViewById(R.id.emailEdit);
-        
+
+        meetingHeader = (TextView) findViewById(R.id.meetingHeader);
+        startPicker = (TimePicker) findViewById(R.id.startPicker);
+        endPicker = (TimePicker) findViewById(R.id.endPicker);
+        titleEdit = (EditText) findViewById(R.id.titleEdit);
+        nameEdit = (EditText) findViewById(R.id.nameEdit);
+        emailEdit = (EditText) findViewById(R.id.emailEdit);
+
         startPicker.setIs24HourView(true);
         endPicker.setIs24HourView(true);
 
         Bundle extras = getIntent().getExtras();
-        
-        if ( extras != null ) {
+        if (extras != null) {
             Time start = new Time();
-            // Use full date if we've it. Otherwise, use day and hour from the bundle.
-            String startStr = extras.getString("start");
+            // Use full date if we've it. Otherwise, use day and hour
+            String startStr = extras.getString(EXTRA_START_TIME);
             if (startStr != null) {
                 start.parse(startStr);
             } else {
-                start.setJulianDay(extras.getInt("day"));
-                start.hour = extras.getInt("hour");
+                start.setJulianDay(extras.getInt(EXTRA_DAY));
+                start.hour = extras.getInt(EXTRA_START_HOUR);
                 start.minute = 0;
             }
+
             Time end = new Time();
-            // Use full date if we've it. Otherwise, use day and hour from the bundle.
-            String endStr = extras.getString("end");
+            // Use full date if we've it. Otherwise set the end to one hour
+            // after start
+            String endStr = extras.getString(EXTRA_END_TIME);
             if (endStr != null) {
                 end.parse(endStr);
             } else {
-                end.setJulianDay(extras.getInt("day"));
-                end.hour = extras.getInt("hour")+1;
+                end.setJulianDay(extras.getInt(EXTRA_DAY));
+                end.hour = extras.getInt(EXTRA_START_HOUR) + 1;
                 if (end.hour > 23)
                     end.hour = 0;
                 end.minute = 0;
@@ -78,23 +87,13 @@ public class MeetingActivity extends Activity {
 
             startPicker.setCurrentHour(start.hour);
             startPicker.setCurrentMinute(start.minute);
-            
-            // One hour meeting by default
+
             endPicker.setCurrentHour(end.hour);
             endPicker.setCurrentMinute(end.minute);
-            
-            if ( extras.getString("title") != null ) {
-                titleEdit.setText(extras.getString("title"));
-            }
-            
-            if ( extras.getString("name") != null ) {
-                nameEdit.setText(extras.getString("name"));
-            }
-            
-            if ( extras.getString("email") != null ) {
-                emailEdit.setText(extras.getString("email"));
-            }
 
+            titleEdit.setText(extras.getString(EXTRA_TITLE));
+            nameEdit.setText(extras.getString(EXTRA_CONTACT_NAME));
+            emailEdit.setText(extras.getString(EXTRA_CONTACT_EMAIL));
         }
     }
 }
--- src/org/androidaalto/bookingroom/view/WeekView.java
+++ src/org/androidaalto/bookingroom/view/WeekView.java
@@ -29,7 +29,6 @@ import org.androidaalto.bookingroom.R;
 import org.androidaalto.bookingroom.logic.MeetingInfo;
 import org.androidaalto.bookingroom.logic.MeetingManager;
 import org.androidaalto.bookingroom.logic.UserInfo;
-import org.androidaalto.bookingroom.model.User;
 
 import android.content.Context;
 import android.content.Intent;
@@ -1140,23 +1139,22 @@ public class WeekView extends View {
         }
     }
 
-    /**
-     * @param mSelectedMeetingInfo2 Edit an existing meeting
-     */
     private void switchToMeetingView() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.putExtra("day", mSelectionDay);
-        intent.putExtra("hour", mSelectionHour);
+        intent.putExtra(MeetingActivity.EXTRA_DAY, mSelectionDay);
+        intent.putExtra(MeetingActivity.EXTRA_START_HOUR, mSelectionHour);
 
         if (mSelectedMeetingInfo != null) {
             UserInfo user = mSelectedMeetingInfo.getUser();
             if (user != null) {
-                intent.putExtra("name", user.getName());
-                intent.putExtra("email", user.getEmail());
+                intent.putExtra(MeetingActivity.EXTRA_CONTACT_NAME, user.getName());
+                intent.putExtra(MeetingActivity.EXTRA_CONTACT_EMAIL, user.getEmail());
             }
-            intent.putExtra("title", mSelectedMeetingInfo.getTitle());
-            intent.putExtra("start", mSelectedMeetingInfo.getStart().format2445());
-            intent.putExtra("end", mSelectedMeetingInfo.getEnd().format2445());
+            intent.putExtra(MeetingActivity.EXTRA_TITLE, mSelectedMeetingInfo.getTitle());
+            intent.putExtra(MeetingActivity.EXTRA_START_TIME, mSelectedMeetingInfo.getStart()
+                    .format2445());
+            intent.putExtra(MeetingActivity.EXTRA_END_TIME, mSelectedMeetingInfo.getEnd()
+                    .format2445());
         }
 
         intent.setClassName(mContext, MeetingActivity.class.getName());
