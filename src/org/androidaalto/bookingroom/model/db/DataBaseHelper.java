998aca2a689141b8a0a76148302521186f733a0b
Ramon <oni.pool@gmail.com>
2012-05-13 18:55:38 +0300

    Use constants for DB tables

--- src/org/androidaalto/bookingroom/model/db/DataBaseHelper.java
+++ src/org/androidaalto/bookingroom/model/db/DataBaseHelper.java
@@ -30,7 +30,7 @@ import android.util.Log;
 public class DataBaseHelper extends SQLiteOpenHelper {
     private static final String TAG = "DataBaseHelper";
 
-    private static final String DB_NAME = "booking.db";
+    private static final String DB_NAME = null;
 
     // Remember to increase whenever you want to call onUpdate
     private static final int DB_VERSION = 1;
@@ -93,6 +93,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public synchronized void close() {
         Log.w(TAG,"DataBaseHelper.close()");
         super.close();
+    }
+
+    /**
+     * Deletes the entire database
+     */
+    public static void clean() {
+        // Since we use an in-memory DB we just need to close it.
+        instance.close();
     }
 
 }
--- src/org/androidaalto/bookingroom/model/db/UserDb.java
+++ src/org/androidaalto/bookingroom/model/db/UserDb.java
@@ -27,6 +27,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class UserDb {
+    private static final String USER_TABLE = "user";
+
     public static int getUserCount() {
         SQLiteDatabase db = DataBaseHelper.getInstance().getReadableDatabase();
         Cursor cursor = null;
@@ -119,7 +121,7 @@ public class UserDb {
         value.put("is_admin", user.isAdmin());
         value.put("salt", user.getSalt());
         value.put("password", user.getPassword());
-        db.insert("user", null, value);
+        db.insert(USER_TABLE, null, value);
         return UserDb.get(user.getEmail());
     }
 
@@ -134,7 +136,7 @@ public class UserDb {
         value.put("email", user.getEmail());
         value.put("salt", user.getSalt());
         value.put("password", user.getPassword());
-        db.update("user", value, "id = ?", new String[] { "" + user.getId() });
+        db.update(USER_TABLE, value, "id = ?", new String[] { "" + user.getId() });
         return UserDb.get(user.getId());
     }
 }
